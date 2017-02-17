e11d9d80d775d37b4426887105dd9a8270ca7593
Jonathan Sebastian <jojosebseb@gmail.com>
2017-02-17 21:13:55 +0700

    v 0.2

---
+++ script/jquery.js
@@ -0,0 +1,22 @@
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top +50
+        }, 300);
+        return false;
+      }
+    }
+  });
+});
+
+$('.language-handle').on('click', function(){
+  $('.language-drop').toggleClass('active');
+})
+
+$('.mobile-handle').on('click', function(){
+  $('.navigation').toggleClass('active');
+})
